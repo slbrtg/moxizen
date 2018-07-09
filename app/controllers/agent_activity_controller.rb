fd59e8725d85a20e89271364c875d58f70bf4788
slbrtg <slbrtg@gmail.com>
2018-07-09 16:57:53 -0700

    added methods to retrieve individual agent data over time

--- app/controllers/agent_activity_controller.rb
+++ app/controllers/agent_activity_controller.rb
@@ -1,25 +1,85 @@
 class AgentActivityController < ApplicationController
 
-  # Should run automatically as a cron job, controller method is around for dev purposes
+  # save_activity should run automatically as a cron job, controller method is around for dev purposes
   def save_activity
     AgentActivity.save_activity
   rescue StandardError => e
     puts e
   end
 
+  # Returns agent id by using the agent name passed in header
+  def get_agent_id
+    agent_id = Agent.where('name = ?', request.headers['HTTP_AGENT_NAME'])[0].id
+    puts "AGENT_ID: #{agent_id}"
+    agent_id
+  end
+
+  # Methods that return individual agent data
   def one_day
-    puts AgentActivity.where('DATE(created_at) = ?', Date.today-1)
+    puts AgentActivity.where('DATE(created_at) = ? and agent_id = ?', Date.today-1, get_agent_id).length
+  rescue StandardError => e
+    puts e
   end
 
-  def one_week
-    puts AgentActivity.where('DATE(created_at) >= ?', Date.today-7).length
+  def seven_days
+    puts AgentActivity.where('DATE(created_at) >= ? and agent_id = ?', Date.today-7, get_agent_id).length
+  rescue StandardError => e
+    puts e
   end
 
-  def one_month
-    puts AgentActivity.where('DATE(created_at) >= ?', Date.today-30).length
+  def thirty_days
+    puts AgentActivity.where('DATE(created_at) >= ? and agent_id = ?', Date.today-30, get_agent_id).length
+  rescue StandardError => e
+    puts e
+  end
+
+  def sixty_days
+    puts AgentActivity.where('DATE(created_at) >= ? and agent_id = ?', Date.today-60, get_agent_id).length
+  rescue StandardError => e
+    puts e
+  end
+
+  def ninety_days
+    puts AgentActivity.where('DATE(created_at) >= ? and agent_id = ?', Date.today-90, get_agent_id).length
+  rescue StandardError => e
+    puts e
+  end
+
+  def half_year
+    puts AgentActivity.where('DATE(created_at) >= ? and agent_id = ?', Date.today-180, get_agent_id).length
+  rescue StandardError => e
+    puts e
   end
 
   def one_year
+    puts AgentActivity.where('DATE(created_at) >= ? and agent_id = ?', Date.today-365, get_agent_id).length
+  rescue StandardError => e
+    puts e
+  end
+
+
+  # Methods that return all agent data
+  def one_day_all
+    puts AgentActivity.where('DATE(created_at) = ?', Date.today-1).length
+  rescue StandardError => e
+    puts e
+  end
+
+  def seven_days_all
+    puts AgentActivity.where('DATE(created_at) >= ?', Date.today-7).length
+  rescue StandardError => e
+    puts e
+  end
+
+  def thirty_days_all
+    puts AgentActivity.where('DATE(created_at) >= ?', Date.today-30).length
+  rescue StandardError => e
+    puts e
+  end
+
+  def one_year_all
     puts AgentActivity.where('DATE(created_at) >= ?', Date.today-365).length
+  rescue StandardError => e
+    puts e
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,27 @@
 Rails.application.routes.draw do
+  # Non-production, used to test Zendesk API
   get '/api/talk/account_overview' => 'talk#account_overview'
   get '/api/talk/agents_activity' => 'talk#agents_activity'
 
+  # Saves agents to the DB, can also be used to update in case there is a new agent
   get '/api/agents/save_agents' => 'agent#save_agents'
 
+  # Currently used to save every agent's activity daily
   get '/api/activity/save_activity' => 'agent_activity#save_activity'
+
+  # The Meat and Potatoes, retrieves the stats by day, week, month, and year
+  get '/api/activity/one_day_all' => 'agent_activity#one_day_all'
+  get '/api/activity/one_week_all' => 'agent_activity#one_week_all'
+  get '/api/activity/one_month_all' => 'agent_activity#one_month_all'
+  get '/api/activity/one_year_all' => 'agent_activity#one_year_all'
+
   get '/api/activity/one_day' => 'agent_activity#one_day'
-  get '/api/activity/one_week' => 'agent_activity#one_week'
-  get '/api/activity/one_month' => 'agent_activity#one_month'
+  get '/api/activity/seven_days' => 'agent_activity#seven_days'
+  get '/api/activity/thirty_days' => 'agent_activity#thirty_days'
+  get '/api/activity/sixty_days' => 'agent_activity#sixty_days'
+  get '/api/activity/ninety_days' => 'agent_activity#ninety_days'
   get '/api/activity/one_year' => 'agent_activity#one_year'
 
+  # Saves the account's daily activity
   get '/api/account_activity/save_account_activity' => 'account_activity#save_account_activity'
 end
